6f16d6782c48f6bb908a3be397daee1e2bb4d7ca
ahseneremmouche <ahsene.remmouche@outlook.com>
2020-12-20 12:16:27 +0100

    Add API

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+//* Get Quote From API forismatic.com
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        getQuote();
+        console.log('Whoops, no quote',err);
+    }
+}
+
+//*On Load
+getQuote();
